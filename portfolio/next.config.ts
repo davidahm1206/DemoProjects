265d04578f791a4f8108324f8a33c32629ec65a7
davidahm1206 <david.hamdan1206@gmail.com>
2026-04-23 23:08:45 +0200

    feat: configure static site generation and automate GitHub Pages deployment workflow

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/DemoProjects',
+  assetPrefix: '/DemoProjects/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
